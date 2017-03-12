28d5e37e2a21f0ec452f4c48c62c1d867262cd00
吉书天 <237840271@qq.com>
2017-03-12 16:49:01 +0800

    'zz'

---
+++ js/rem.js
@@ -0,0 +1,14 @@
+var designWidth = 750;
+
+function resize(){
+
+	var width = document.documentElement.clientWidth;
+    var fontSize = (width/designWidth)*100;
+
+    document.documentElement.style.fontSize = fontSize + "px";
+}
+
+resize();
+
+window.onresize = resize;
+
